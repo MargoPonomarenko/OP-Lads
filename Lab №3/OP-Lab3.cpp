dcd37e5b6ab8260a8381b2b88fa8916e3fbf66e6
MargoPonomarenko <59797379+MargoPonomarenko@users.noreply.github.com>
2021-10-29 18:17:16 +0300

    Add files via upload

---
+++ Lab №3/OP-Lab3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath> 
+#include <locale>
+using namespace std;
+int main()
+{
+    float a, xn, x_next;
+    setlocale(LC_CTYPE, "ukr");
+    cout << " Обчислити значення квадратного кореня iз числа a > 0 iз заданою точнiстю е на основi рекурентного спiввiдношення. \n"; // умова задачі
+    cout << " Введiть значення a>0 \n";
+    cin >> a;
+    if (a <= 0)
+    {
+        exit(1);
+    }
+    xn = a / 2;
+    x_next = 0.5 * (xn + a / xn);
+    while (abs(x_next - xn) > pow(10, -5))
+    {
+        xn = x_next;
+        x_next = 0.5 * (xn + a / xn);
+    }
+    cout << x_next;
+}
