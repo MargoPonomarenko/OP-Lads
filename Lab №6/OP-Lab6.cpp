94af1b1f23aaa8ee96b11b0ea1650fcfa5877fe4
MargoPonomarenko <59797379+MargoPonomarenko@users.noreply.github.com>
2021-12-02 23:51:56 +0200

    Add files via upload

---
+++ Lab №6/OP-Lab6.cpp
@@ -0,0 +1,30 @@
+﻿#include<math.h>
+#define _USE_MATH_DEFINES
+#include<iostream>
+using namespace std;
+float a, b, U, V, W;
+
+int main() {
+    Init();
+    Solution();
+    Browse();
+    system("pause");
+}
+
+void Init() {
+    cout << "insert a and b value" << endl;
+    cin >> a;
+    cin >> b;
+}
+
+void Solution() {
+    U = min(a, b);
+    V = min(a * b, a + b);
+    W = min(U + pow(V,2), M_PI);
+}
+
+void Browse() {
+    cout << "U value = " << U << endl;
+    cout << "V value = " << V << endl;
+    cout << "W value = " << W << endl;
+}
